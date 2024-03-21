df6c9ed3dc60de307956bd44aef3474ae4b42d06
sandeshsitaula <navaltajohan@gmail.com>
2024-03-21 20:25:35 +0545

    app.jsx content cleared

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,14 +2,12 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import GoogleLogin from './components/googleLogin'
-import FacebookLogin from './components/facebookLogin'
+
 function App() {
 
   return (
     <>
-     <GoogleLogin />
-     <FacebookLogin />
+
     </>
   )
 }
