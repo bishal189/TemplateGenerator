d74567e6a4d86d41e1cca5257863b8e462040f54
sandeshsitaula <navaltajohan@gmail.com>
2024-03-21 17:52:26 +0545

    added frotend for google login

---
+++ frontend/src/components/googleLogin.jsx
@@ -0,0 +1,48 @@
+import {useEffect} from 'react'
+
+const GoogleLogin = () => {
+  useEffect(() => {
+
+    // Function to handle the Google Sign-In response
+    const handleCredentialResponse = async (response) => {
+      if (response.credential) {
+        var id_token = response.credential;
+        console.log(id_token)
+        const apiResponse = await fetch(`${import.meta.env.VITE_BACKEND_URL}/api/auth/google`, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ 'id_token': id_token }),
+        });
+
+    const data = await apiResponse.json(); // or response.text() depending on the response content type
+    // Process the data
+    console.log(data);      }
+    };
+
+    // Load Google Sign-In library and configure it
+    window.onload = function () {
+      window.google.accounts.id.initialize({
+        client_id: '322302178356-qeiht1u6u98b1n42rku6aen4ficbjp6r.apps.googleusercontent.com',
+        callback: handleCredentialResponse, // Set the callback function
+      });
+      window.google.accounts.id.renderButton(
+        document.getElementById('g_id_signin'), // Specify the element ID
+        {
+          theme: 'outline',
+          type: 'standard',
+          size: 'large',
+        }
+      );
+    };
+   console.log( `${import.meta.env.VITE_BACKEND_URL}/api/auth/google`)
+  }, []); // Ensure that this effect runs only once on component mount
+
+  return (
+
+      <div id="g_id_signin"></div>
+  );
+};
+
+export default GoogleLogin;
