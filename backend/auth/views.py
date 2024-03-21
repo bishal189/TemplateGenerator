4781137a1549217fdaeb77b0b7d842d86078b5bd
sandeshsitaula <navaltajohan@gmail.com>
2024-03-21 20:01:49 +0545

    cors setup

--- backend/auth/views.py
+++ backend/auth/views.py
@@ -1,3 +1,44 @@
 from django.shortcuts import render
-
+import json
+from google.oauth2 import id_token
+from google.auth.transport import requests
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.response import Response
+from rest_framework import status
+import os
 # Create your views here.
+# logiin with google
+@api_view(['POST'])
+@csrf_exempt
+def googleoauth(request):
+
+        try:
+            data=json.loads(request.body)
+            id_token_data = data['id_token']
+
+            CLIENT_ID = os.getenv('GOOGLE_CLIENT_ID')
+            idinfo = id_token.verify_oauth2_token(id_token_data, requests.Request(), CLIENT_ID)
+            print(idinfo)
+            if idinfo['iss'] not in ['accounts.google.com', 'https://accounts.google.com']:
+                raise ValueError('Wrong issuer.')
+
+            # Here you can create a user session or perform any other necessary actions.
+            username=idinfo['name']
+            email=idinfo['email']
+            first_name=idinfo['given_name']
+            last_name=idinfo['family_name']
+            '''
+            user,created=Account.objects.get_or_create(email=email,username=username,first_name=first_name,last_name=last_name)
+            user.is_active=True
+            user.save()
+            auth.login(request,user)
+            response={
+                'message':'Login Successfull',
+                'user':RegistrationSerializer(user).data,
+                'status':status.HTTP_200_OK,
+            }'''
+            print(username,email,first_name,last_name)
+            return Response(response)
+        except Exception as e:
+            print(e)
+            return Response({'status':False})
--- backend/templateProject/settings.py
+++ backend/templateProject/settings.py
@@ -9,7 +9,11 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
+import os
+from dotenv import load_dotenv
 
+# Load environment variables from .env file
+load_dotenv()
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +29,7 @@ SECRET_KEY = 'django-insecure-pg-p2ibdsmkafmk29o$o9(q_i$tgd((0#!m_86djy2oug^(hyz
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,9 +41,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+        'corsheaders',
+        'rest_framework',
+        'auth',
+
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -65,6 +74,11 @@ TEMPLATES = [
             ],
         },
     },
+]
+CORS_ALLOW_CREDENTIALS = True
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5000",
 ]
 
 WSGI_APPLICATION = 'templateProject.wsgi.application'
