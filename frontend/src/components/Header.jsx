7b439ed456c980a4abcb5b2306753195ba08722d
Subin Budhathoki <subinbudhathoki09@gmail.com>
2024-03-22 16:52:59 +0545

    navbar completion

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,16 +1,12 @@
-import  { useState } from 'react';
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
+import { useState } from "react";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
 import { IoIosSearch } from "react-icons/io";
-import './Header.css'; 
+import { GoArrowUpRight } from "react-icons/go";
 
-
-
-      
-      
-   
+import "./Header.css";
 
 function Header() {
   const [showBusinessDropdown, setShowBusinessDropdown] = useState(false);
@@ -25,28 +21,41 @@ function Header() {
   };
 
   return (
-    <Navbar bg="light" expand="lg" className="bg-body-tertiary" data-bs-theme="light">
+    <Navbar
+      bg="light"
+      expand="lg"
+      className="bg-body-tertiary"
+      data-bs-theme="light"
+    >
       <Container>
         <Navbar.Brand href="#home">legaltemplates.</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <NavDropdown 
-              title="Business" 
+            <NavDropdown
+              title="Business"
               id="business-nav-dropdown"
               show={showBusinessDropdown}
               onMouseEnter={() => setShowBusinessDropdown(true)}
               onMouseLeave={() => setShowBusinessDropdown(false)}
             >
-              <NavDropdown.Item href="">Non-Disclosure Agreement</NavDropdown.Item>
+              <NavDropdown.Item href="">
+                Non-Disclosure Agreement
+              </NavDropdown.Item>
               <NavDropdown.Item href="">Employment Contract</NavDropdown.Item>
               <NavDropdown.Item href="">Release of Liability</NavDropdown.Item>
               <NavDropdown.Item href="">Purchase Agreement</NavDropdown.Item>
               <NavDropdown.Item href="">Partnership Agreement</NavDropdown.Item>
-              <NavDropdown.Item href="">View all forms</NavDropdown.Item>
+              <NavDropdown.Item
+                href=""
+                style={{ display: "flex", alignItems: "center" }}
+              >
+                View all forms
+                <GoArrowUpRight style={{ color: "blue", marginLeft: "5px" }} />
+              </NavDropdown.Item>
             </NavDropdown>
-            <NavDropdown 
-              title="Real Estate" 
+            <NavDropdown
+              title="Real Estate"
               id="real-estate-nav-dropdown"
               show={showRealStateDropdown}
               onMouseEnter={() => setShowRealStateDropdown(true)}
@@ -57,24 +66,38 @@ function Header() {
               <NavDropdown.Item href="">Lease Termination</NavDropdown.Item>
               <NavDropdown.Item href="">Quitclaim Deed</NavDropdown.Item>
               <NavDropdown.Item href="">Rental Application</NavDropdown.Item>
-              <NavDropdown.Item href="">View all forms</NavDropdown.Item>
+              <NavDropdown.Item
+                href=""
+                style={{ display: "flex", alignItems: "center" }}
+              >
+                View all forms
+                <GoArrowUpRight style={{ color: "blue", marginLeft: "5px" }} />
+              </NavDropdown.Item>
             </NavDropdown>
-            <NavDropdown 
-              title="Estate Planning" 
+            <NavDropdown
+              title="Estate Planning"
               id="estate-nav-dropdown"
               show={showEstateDropdown}
               onMouseEnter={() => setShowEstateDropdown(true)}
               onMouseLeave={() => setShowEstateDropdown(false)}
             >
               <NavDropdown.Item href="">Power of Attorney</NavDropdown.Item>
-              <NavDropdown.Item href="">Last Will and Testament</NavDropdown.Item>
+              <NavDropdown.Item href="">
+                Last Will and Testament
+              </NavDropdown.Item>
               <NavDropdown.Item href="">Advance Directive</NavDropdown.Item>
               <NavDropdown.Item href="">Living Will</NavDropdown.Item>
               <NavDropdown.Item href="">Do not Resuscitate</NavDropdown.Item>
-              <NavDropdown.Item href="">View all forms</NavDropdown.Item>
+              <NavDropdown.Item
+                href=""
+                style={{ display: "flex", alignItems: "center" }}
+              >
+                View all forms
+                <GoArrowUpRight style={{ color: "blue", marginLeft: "5px" }} />
+              </NavDropdown.Item>
             </NavDropdown>
-            <NavDropdown 
-              title="Personal" 
+            <NavDropdown
+              title="Personal"
               id="personal-nav-dropdown"
               show={showPersonalDropdown}
               onMouseEnter={() => setShowPersonalDropdown(true)}
@@ -86,36 +109,63 @@ function Header() {
               <NavDropdown.Item href="">Divorce Agreement</NavDropdown.Item>
               <NavDropdown.Item href="">Child Travel Consent</NavDropdown.Item>
               <NavDropdown.Item href="">Loan Agreement</NavDropdown.Item>
-              <NavDropdown.Item href="">View all forms</NavDropdown.Item>
+              <NavDropdown.Item
+                href=""
+                style={{ display: "flex", alignItems: "center" }}
+              >
+                View all forms
+                <GoArrowUpRight style={{ color: "blue", marginLeft: "5px" }} />
+              </NavDropdown.Item>
             </NavDropdown>
           </Nav>
           <Nav>
-  <Nav.Link onClick={toggleSearch}><IoIosSearch size={24} /></Nav.Link>
-  {showSearch && (
-    <NavDropdown  id="search-nav-dropdown" show={showSearch} align="end" onClick={(e) => e.stopPropagation()}>
-      <div className="search-box">
-        <div className="search-input">
-          <div className="icon-placeholder">
-            <IoIosSearch size={28} />
-          </div>
-          <input type="text" placeholder="Search Documents and Forms(e.g. lease agreement)" />
-          <button className="w-1/6 mx-1 py-2 font-bold rounded-sm bg-violet-600 border-2 border-violet-600 hover:bg-white hover:text-violet-600 transition-all">
-              Search
-            </button>
-        </div>
-      </div>
-    </NavDropdown>
-  )}
-</Nav>
-
+            <Nav.Link onClick={toggleSearch}>
+              <IoIosSearch size={24} />
+            </Nav.Link>
+            {showSearch && (
+              <NavDropdown
+                id="search-nav-dropdown"
+                show={showSearch}
+                align="end"
+                onClick={(e) => e.stopPropagation()}
+              >
+                <div className="search-box">
+                  <div className="search-input">
+                    <div className="icon-placeholder">
+                      <IoIosSearch size={28} />
+                    </div>
+                    <input
+                      type="text"
+                      placeholder="Search Documents and Forms(e.g. lease agreement)"
+                    />
+                    <button className="w-1/6 mx-1 py-2 font-bold rounded-sm bg-violet-600 border-2 border-violet-600 hover:bg-white hover:text-violet-600 transition-all">
+                      Search
+                    </button>
+                  </div>
+                </div>
+              </NavDropdown>
+            )}
+          </Nav>
 
           <Nav>
-            <NavDropdown title="Support" id="basic-nav-dropdown" show={showSupportDropdown}
-            onMouseEnter={() => setShowSupportDropdown(true)}
-            onMouseLeave={() => setShowSupportDropdown(false)}>
+            <NavDropdown
+              title="Support"
+              id="basic-nav-dropdown"
+              show={showSupportDropdown}
+              onMouseEnter={() => setShowSupportDropdown(true)}
+              onMouseLeave={() => setShowSupportDropdown(false)}
+            >
               <NavDropdown.Item href="">About Us</NavDropdown.Item>
               <NavDropdown.Item href="">Chat Online</NavDropdown.Item>
               <NavDropdown.Item href="">Contact Us</NavDropdown.Item>
+             
+              <NavDropdown.Item disabled>
+  <div style={{ fontSize: 'small' }}>
+    Monday - Friday | 9AM - 7PM <br /> EDT
+    (855) 335-9779
+  </div>
+</NavDropdown.Item>
+
             </NavDropdown>
             <Nav.Link eventKey={2} href="#">
               Sign in
