3c1f483142b6f13d51faa9c14cf7b0a8244f0a33
sandeshsitaula <navaltajohan@gmail.com>
2024-03-22 13:36:55 +0545

    why choose template part ongoing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,12 +5,14 @@ import "./App.css";
 import Header from "./components/Header";
 import Home from "./pages/Home";
 import {HowItWorks} from './components/howItWorks'
+import {WhyChooseTemplate} from './components/chooseTemplate'
 function App() {
   return (
     <div className="App">
       <Header />
       <Home />
       <HowItWorks />
+      <WhyChooseTemplate />
     </div>
   );
 }
