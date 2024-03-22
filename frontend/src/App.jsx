d25d7bf15f44d3f32b68466e951080682a2c41e9
sandeshsitaula <navaltajohan@gmail.com>
2024-03-22 12:25:10 +0545

    how it works part completed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,12 +4,13 @@ import 'tailwindcss/tailwind.css';
 import "./App.css";
 import Header from "./components/Header";
 import Home from "./pages/Home";
-
+import {HowItWorks} from './components/howItWorks'
 function App() {
   return (
     <div className="App">
       <Header />
       <Home />
+      <HowItWorks />
     </div>
   );
 }
---
+++ frontend/src/components/howItWorks.jsx
@@ -0,0 +1,43 @@
+import "./css/howItWorks.css"
+import Works1 from '../assets/works1.webp'
+import Works2 from '../assets/works2.webp'
+import Works3 from '../assets/works3.webp'
+export function HowItWorks(){
+return(
+    <>
+    <section className="section-works">
+    <div className="wrap">
+    <h2>How It Works</h2>
+    <div className="inner">
+        <div className="block">
+            <div>
+<img src={Works1} alt="choose from legal forms list" loading="lazy" decoding="async" width="204" height="180" />
+<h3>Choose Your legal Form</h3>
+<p>Browse our 160+ free online legal contracts & documents</p>
+
+           </div>
+        </div>
+
+           <div className="block">
+            <div>
+<img src={Works2} alt="choose from legal forms list" loading="lazy" decoding="async" width="204" height="180" />
+<h3>Answer Simple Questions</h3>
+<p>Our contract creator will turn your answers into a customized legal form</p>
+
+           </div>
+        </div>
+
+           <div className="block">
+            <div>
+<img src={Works3} alt="choose from legal forms list" loading="lazy" decoding="async" width="204" height="180" />
+<h3>Sign Your Document</h3>
+<p>Download and print your customized legal document</p>
+           </div>
+        </div>
+
+    </div>
+    </div>
+    </section>
+    </>
+)
+}
