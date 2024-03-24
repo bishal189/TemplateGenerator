07c42df0fafe9dc4ef264db3d450a1b536419e68
sandeshsitaula <navaltajohan@gmail.com>
2024-03-24 20:59:15 +0545

    one small issue fixed

--- frontend/src/components/Template/DatePicker.jsx
+++ frontend/src/components/Template/DatePicker.jsx
@@ -1,28 +1,29 @@
-import React, { useState } from 'react';
-import { useState } from 'react';
-import PropTypes from 'prop-types'; // Import PropTypes
-import DatePicker from 'react-datepicker';
-import 'react-datepicker/dist/react-datepicker.css';
-import './InputField.css';
+import React, { useState } from "react";
+import PropTypes from "prop-types"; // Import PropTypes
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import "./InputField.css";
 
-const Datepicker = (props) => {
-
-  const handleDateChange = (date) => {
-    props.setSelectedDate(date)
-      const formattedDate = `${(date.getMonth() + 1).toString().padStart(2, '0')}-${date.getDate().toString().padStart(2, '0')}-${date.getFullYear()}`;
-      console.log(formattedDate)
-    props.setClientData((prev)=>({
+const Datepicker = props => {
+  const handleDateChange = date => {
+    props.setSelectedDate(date);
+    const formattedDate = `${(date.getMonth() + 1)
+      .toString()
+      .padStart(2, "0")}-${date
+      .getDate()
+      .toString()
+      .padStart(2, "0")}-${date.getFullYear()}`;
+    console.log(formattedDate);
+    props.setClientData(prev => ({
       ...prev,
-      'bdate':formattedDate
+      bdate: formattedDate
     }));
-
   };
 
   return (
     <section className="form-section_question-section__1E7Nj">
       <h3>
         <div className="form-section_question-ask__2z7Oj">{props.title}</div>
-
       </h3>
       <div className="text-input_container__crGA3">
         <div>
@@ -30,24 +31,21 @@ const Datepicker = (props) => {
             <DatePicker
               name={props.field}
               selected={props.selectedDate}
-
               onChange={handleDateChange}
               dateFormat="MM/dd/yyyy" // Adjust as needed
               placeholderText="Enter Date of Birth"
               className="text-input_input__lnI9P date-picker-input"
             />
           </div>
-          <div className="Input-error-message-6da351"></div>
+          <div className="Input-error-message-6da351" />
         </div>
       </div>
     </section>
   );
 };
 
-
 Datepicker.propTypes = {
   title: PropTypes.string.isRequired // Define the prop types
 };
-
 
 export default Datepicker;
