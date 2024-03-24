dc9a75c748d086ea6de844484d32abb0d31e3e05
Subin Budhathoki <subinbudhathoki09@gmail.com>
2024-03-24 08:45:24 +0545

    changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,14 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import 'tailwindcss/tailwind.css';
-// import Footer from './components/Footer';
-import "./App.css";
-// import Header from "./components/Header";
-// import Home from "./pages/Home";
+
+
 import FirstPage from "./pages/FirstPage";
 
 
 function App() {
   return (
     <div className="App">
-      {/* <Header/>
-      <Home />
-      <Footer /> */}
+      
       <FirstPage/>
 
     </div>
