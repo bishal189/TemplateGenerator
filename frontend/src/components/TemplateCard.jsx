68adc2c0b3090b818f9f8838051f5c67fe8ee1c9
Bishal Poudel <bishal.poudel@crystalsolution.com.np>
2024-03-24 18:15:39 +0545

    color changed to blue

--- frontend/src/components/TemplateCard.jsx
+++ frontend/src/components/TemplateCard.jsx
@@ -2,9 +2,9 @@ import PropTypes from "prop-types";
 
 const TemplateCard = ({ Icon, Text }) => {
   return (
-    <div className="group flex flex-col justify-items-center justify-center border-2 rounded-lg border-violet-200 hover:border-violet-600 transition-all py-4 px-3 mx-1 hover:bg-violet-100">
-      <Icon className="text-5xl w-full text-violet-200 group-hover:text-violet-400 transition-all group-hover:bg-violet-100" />
-      <p className="font-bold text-sm text-gray-600 group-hover:text-gray-900 group-hover:bg-violet-100 transition-all">
+    <div className="group flex flex-col justify-items-center justify-center border-2 rounded-lg border-blue-200 hover:border-blue-600 transition-all py-4 px-3 mx-1 hover:bg-blue-100">
+      <Icon className="text-5xl w-full text-blue-200 group-hover:text-blue-400 transition-all group-hover:bg-blue-100" />
+      <p className="font-bold text-sm text-gray-600 group-hover:text-gray-900 group-hover:bg-blue-100 transition-all">
         {Text}
       </p>
     </div>
