a64035e220b02edd1e62d503ec592d6f68360cc0
bishal189 <bishalmurmu150@gmail.com>
2024-03-26 11:35:36 +0545

    added the popup model for rename

--- frontend/src/components/Template/Navbar.jsx
+++ frontend/src/components/Template/Navbar.jsx
@@ -5,6 +5,7 @@ import { useState } from 'react';
 const Navbar = () => {
     const [showTooltip, setShowTooltip] = useState(false);
     const [showPopup, setShowPopup] = useState(false);
+    const [model, setModel] = useState(false);
 
     const togglePopup = () => {
         setShowPopup(!showPopup);
@@ -17,6 +18,16 @@ const Navbar = () => {
     const handleMouseLeave = () => {
         setShowTooltip(false);
     };
+
+    const handleRenameClick = () => {
+        setModel(true);
+        togglePopup()
+    };
+    const removeRenameClick = () => {
+        setModel(false);
+    };
+
+
 
 
 
@@ -108,7 +119,7 @@ const Navbar = () => {
             </div>
 
             <div className={showPopup ? 'popup show' : 'popup'}>
-                <div className='rename'>
+                <div className='rename'  onClick={handleRenameClick} >
                     <div className='rename1'>
                         <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M18 4V3C18 2.45 17.55 2 17 2H5C4.45 2 4 2.45 4 3V7C4 7.55 4.45 8 5 8H17C17.55 8 18 7.55 18 7V6H19V10H9V21C9 21.55 9.45 22 10 22H12C12.55 22 13 21.55 13 21V12H21V4H18Z" fill="#283250" fill-opacity="0.54"></path></svg>
                     </div>
@@ -147,6 +158,24 @@ const Navbar = () => {
 
 
             </div>
+
+
+            {model && (
+                <div className='popup-container'>
+                    <div className='internalpopup-container'>
+                        <button className='close-btn' onClick={removeRenameClick}>
+                            <svg width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M7.41421 6L11.7071 1.70711C12.0976 1.31658 12.0976 0.683417 11.7071 0.292893C11.3166 -0.0976308 10.6834 -0.0976308 10.2929 0.292893L6 4.58579L1.70711 0.292893C1.31658 -0.0976308 0.683417 -0.0976308 0.292893 0.292893C-0.0976308 0.683417 -0.0976308 1.31658 0.292893 1.70711L4.58579 6L0.292893 10.2929C-0.0976308 10.6834 -0.0976308 11.3166 0.292893 11.7071C0.488408 11.9026 0.744227 12 1 12C1.25577 12 1.51159 11.9026 1.70711 11.7071L6 7.41421L10.2929 11.7071C10.4884 11.9026 10.7442 12 11 12C11.2558 12 11.5116 11.9026 11.7071 11.7071C12.0976 11.3166 12.0976 10.6834 11.7071 10.2929L7.41421 6Z" fill="#283250"></path></svg>
+                        </button>
+                        <div className='heading'><h2>Rename File</h2></div>
+                        
+                        <input className='text-input' type="text" placeholder="Enter new name" />
+                        <div className='btn-container'>
+                            <button className='cancel-btn' onClick={togglePopup}>Cancel</button>
+                            <button className='save-btn'>Save</button>
+                        </div>
+                    </div>
+                </div>
+            )}
         </div>
     );
 };
